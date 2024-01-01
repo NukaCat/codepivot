39a0fa5eb4ddc29c0ab70956dc005786759454af
NukaCat <sqrt19@gmail.com>
2024-01-02 00:09:03 +0400

    Refactoring

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -20,18 +21,21 @@ type Task struct {
 	Input  string
 }
 
-func parseTaskFile(filePath string) Task {
+func loadTask(taskName string) (Task, error) {
+	dataDir := "data"
+	filePath := filepath.Join(dataDir, taskName+".txt")
+
 	file, err := os.Open(filePath)
 	defer file.Close()
 	if err != nil {
-		panic(fmt.Sprintf("Failed to open task %v %v", filePath, err))
+		return Task{}, fmt.Errorf("Failed to open task task %v %v", filePath, err)
 	}
 
 	scanner := bufio.NewScanner(file)
 	var task Task
 	var currentSection string
 
-	task.Name = strings.TrimSuffix(filepath.Base(filePath), filepath.Ext(filePath))
+	task.Name = taskName
 	for scanner.Scan() {
 		line := scanner.Text()
 		switch line {
@@ -62,19 +66,20 @@ func parseTaskFile(filePath string) Task {
 	}
 
 	if scanner.Err() != nil {
-		panic(fmt.Sprintf("Failed to read task %v %v", filePath, err))
+		return Task{}, fmt.Errorf("Failed to read task %v %v", filePath, err)
 	}
-	return task
+	return task, nil
 }
 
-func loadTasks() ([]Task, error) {
-	data_dir := "./data"
+func listTaskNames() []string {
+	data_dir := "data"
 	entries, err := os.ReadDir(data_dir)
 	if err != nil {
-		return nil, fmt.Errorf("failed find ./data dir %w", err)
+		fmt.Println("failed to find tasks dir")
+		return make([]string, 0)
 	}
 
-	var tasks []Task
+	var taskNames []string
 	for _, entry := range entries {
 		if entry.IsDir() {
 			continue
@@ -82,225 +87,229 @@ func loadTasks() ([]Task, error) {
 		if filepath.Ext(entry.Name()) != ".txt" {
 			continue
 		}
-		path := filepath.Join(data_dir, entry.Name())
-		task := parseTaskFile(path)
-		tasks = append(tasks, task)
+		taskName := strings.TrimSuffix(filepath.Base(entry.Name()), filepath.Ext(entry.Name()))
+		taskNames = append(taskNames, taskName)
 	}
-	return tasks, nil
+	return taskNames
 }
 
 type User struct {
-	Name      string   `json:"name"`
-	Password  string   `json:"password"`
-	DoneTasks []string `json:"done"`
+	Name     string         `json:"name"`
+	Password string         `json:"password"`
+	Progress map[string]int `json:"progress"`
 }
 
-func loadUsers(filePath string) ([]User, error) {
+func loadUser(userName string) (User, error) {
+	filePath := filepath.Join("users", userName+".json")
 	file, err := os.Open(filePath)
 	if err != nil {
-		return nil, fmt.Errorf("failed to open users file %s %w", filePath, err)
+		return User{}, fmt.Errorf("failed to open user file %s %w", filePath, err)
 	}
 	defer file.Close()
 
 	data, err := io.ReadAll(file)
 	if err != nil {
-		return nil, fmt.Errorf("failed to read users file %s %w", filePath, err)
+		return User{}, fmt.Errorf("failed to read user file %s %w", filePath, err)
 	}
 
-	var users []User
-
-	err = json.Unmarshal(data, &users)
+	var user User
+	err = json.Unmarshal(data, &user)
 	if err != nil {
-		return nil, fmt.Errorf("failed to parse users file %s %w", filePath, err)
+		return User{}, fmt.Errorf("failed to parse user file %s %w", filePath, err)
 	}
 
-	return users, nil
+	if user.Progress == nil {
+		user.Progress = make(map[string]int)
+	}
+
+	return user, nil
 }
 
-func checkCredentials(username string, password string, users []User) bool {
-	for _, user := range users {
-		if user.Name == username && user.Password == password {
-			return true
-		}
-	}
-	return false
-}
+func saveUser(userName string, user User) error {
+	filePath := filepath.Join("users", userName+".json")
 
-func checkUserDoneTask(userName string, taskName string) bool {
-	userPath := userName + ".user"
-	file, err := os.Open(userPath)
+	jsonData, err := json.MarshalIndent(user, "", "  ")
 	if err != nil {
-		return false
+		return err
+	}
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		return fmt.Errorf("failed to create file %s %w", filePath, err)
 	}
 	defer file.Close()
 
-	scanner := bufio.NewScanner(file)
+	file.Write(jsonData)
+	return nil
+}
 
-	for scanner.Scan() {
-		line := scanner.Text()
-		if line == taskName {
-			return true
-		}
+func checkCredentials(username string, password string) bool {
+	user, err := loadUser(username)
+	if err != nil {
+		return false
+	}
+	if user.Name == username && user.Password == password {
+		return true
 	}
 	return false
 }
 
-func markTaskDone(userName string, taskName string) {
-	userPath := userName + ".user"
-	f, err := os.OpenFile(userPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		return
-	}
-	defer f.Close()
+type UserHandlerFunc func(User, http.ResponseWriter, *http.Request)
 
-	_, err = f.WriteString(taskName + "\n")
-}
-
-type UserHandlerFunc func(string, http.ResponseWriter, *http.Request)
-
-func BasicAuth(users []User, handler UserHandlerFunc) http.HandlerFunc {
+func BasicAuth(handler UserHandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		username, password, ok := r.BasicAuth()
-		if !ok || !checkCredentials(username, password, users) {
+		if !ok {
 			w.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
 			http.Error(w, "Unauthorized", http.StatusUnauthorized)
 			return
 		}
-		handler(username, w, r)
+		user, err := loadUser(username)
+		if err != nil {
+			w.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		if user.Name != username || user.Password != password {
+			w.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		handler(user, w, r)
 	}
 }
 
+func MainPage(user User, w http.ResponseWriter, r *http.Request) {
+	var notDoneTasks []Task
+	var doneTasks []Task
+
+	taskNames := listTaskNames()
+
+	for _, taskName := range taskNames {
+		task, err := loadTask(taskName)
+		if err != nil {
+			log.Println("Can't load task %w", err)
+			continue
+		}
+
+		done := user.Progress[taskName] > 0
+		if done {
+			doneTasks = append(doneTasks, task)
+		} else {
+			notDoneTasks = append(notDoneTasks, task)
+		}
+	}
+	type TasksPage struct {
+		DoneTasks    []Task
+		NotDoneTasks []Task
+	}
+
+	indexTmpl := template.Must(template.ParseFiles("index.html"))
+	indexTmpl.Execute(w, TasksPage{DoneTasks: doneTasks, NotDoneTasks: notDoneTasks})
+}
+
+func TaskPage(user User, w http.ResponseWriter, r *http.Request) {
+	taskSegments := strings.Split(r.URL.Path, "/")
+	if len(taskSegments) < 3 {
+		http.Error(w, "No task name", http.StatusBadRequest)
+		return
+	}
+
+	taskName := taskSegments[2]
+	task, err := loadTask(taskName)
+	if err != nil {
+		http.Error(w, "Task not found", http.StatusNotFound)
+		return
+	}
+
+	done := user.Progress[task.Name] > 0
+
+	type TaskPage struct {
+		Task Task
+		Done bool
+	}
+	taskPage := TaskPage{Task: task, Done: done}
+
+	taskTmpl := template.Must(template.ParseFiles("task.html"))
+	taskTmpl.Execute(w, taskPage)
+}
+
+func SubmitPage(user User, w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Not post request", http.StatusBadRequest)
+		return
+	}
+
+	taskSegments := strings.Split(r.URL.Path, "/")
+	if len(taskSegments) < 3 {
+		http.Error(w, "No task name", http.StatusBadRequest)
+		return
+	}
+
+	err := r.ParseForm()
+	if err != nil {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	answer := r.FormValue("answer")
+
+	taskName := taskSegments[2]
+	task, err := loadTask(taskName)
+	if err != nil {
+		http.Error(w, "Failed to load task", http.StatusInternalServerError)
+		return
+	}
+
+	accepted := task.Answer == answer
+	if accepted {
+		user.Progress[taskName] = 1
+		err = saveUser(user.Name, user)
+		if err != nil {
+			http.Error(w, "Failed to update user", http.StatusInternalServerError)
+			return
+		}
+	}
+
+	type SubmitPage struct {
+		TaskName string
+		Accepted bool
+	}
+
+	submitPage := SubmitPage{TaskName: taskName, Accepted: accepted}
+
+	submitTpl := template.Must(template.ParseFiles("submit.html"))
+	submitTpl.Execute(w, submitPage)
+}
+
+func InputFile(user User, w http.ResponseWriter, r *http.Request) {
+	taskSegments := strings.Split(r.URL.Path, "/")
+	if len(taskSegments) < 3 {
+		http.Error(w, "No task name", http.StatusBadRequest)
+		return
+	}
+
+	taskName := taskSegments[2]
+	task, err := loadTask(taskName)
+	if err != nil {
+		http.Error(w, "Can't find task", http.StatusNotFound)
+		return
+	}
+
+	w.Write([]byte(task.Input))
+}
+
 func main() {
-	tasks, err := loadTasks()
-	if err != nil {
-		panic(err)
-	}
-	users, err := loadUsers("users.json")
-	if err != nil {
-		panic(err)
-	}
 
-	index_tmpl := template.Must(template.ParseFiles("index.html"))
-	task_tmpl := template.Must(template.ParseFiles("task.html"))
-	submit_tpl := template.Must(template.ParseFiles("submit.html"))
-
-	http.HandleFunc("/", BasicAuth(users, func(user string, w http.ResponseWriter, r *http.Request) {
-		var notDoneTasks []Task
-		var doneTasks []Task
-		for _, task := range tasks {
-			done := checkUserDoneTask(user, task.Name)
-			if done {
-				doneTasks = append(doneTasks, task)
-			} else {
-				notDoneTasks = append(notDoneTasks, task)
-			}
-		}
-		type TasksPage struct {
-			DoneTasks    []Task
-			NotDoneTasks []Task
-		}
-		index_tmpl.Execute(w, TasksPage{DoneTasks: doneTasks, NotDoneTasks: notDoneTasks})
-	}))
+	http.HandleFunc("/", BasicAuth(MainPage))
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 
-	http.HandleFunc("/input/", BasicAuth(users, func(user string, w http.ResponseWriter, r *http.Request) {
-		taskSegments := strings.Split(r.URL.Path, "/")
-		if len(taskSegments) < 3 {
-			http.Error(w, "No task name", http.StatusBadRequest)
-			return
-		}
+	http.HandleFunc("/input/", BasicAuth(InputFile))
 
-		taskName := taskSegments[2]
-		var task *Task
+	http.HandleFunc("/task/", BasicAuth(TaskPage))
 
-		for idx, _ := range tasks {
-			if tasks[idx].Name == taskName {
-				task = &tasks[idx]
-			}
-		}
-
-		if task == nil {
-			http.Error(w, "Can't find task", http.StatusNotFound)
-			return
-		}
-
-		w.Write([]byte(task.Input))
-	}))
-
-	http.HandleFunc("/task/", BasicAuth(users, func(user string, w http.ResponseWriter, r *http.Request) {
-		taskSegments := strings.Split(r.URL.Path, "/")
-		if len(taskSegments) < 3 {
-			http.Error(w, "No task name", http.StatusBadRequest)
-			return
-		}
-
-		taskName := taskSegments[2]
-		var task *Task
-
-		for idx, _ := range tasks {
-			if tasks[idx].Name == taskName {
-				task = &tasks[idx]
-			}
-		}
-
-		if task == nil {
-			http.Error(w, "Can't find task", http.StatusNotFound)
-			return
-		}
-
-		done := checkUserDoneTask(user, taskName)
-
-		type TaskPage struct {
-			Task Task
-			Done bool
-		}
-		taskPage := TaskPage{Task: *task, Done: done}
-
-		task_tmpl.Execute(w, taskPage)
-	}))
-
-	http.HandleFunc("/submit/", BasicAuth(users, func(user string, w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			http.Error(w, "Not post request", http.StatusBadRequest)
-			return
-		}
-
-		taskSegments := strings.Split(r.URL.Path, "/")
-		if len(taskSegments) < 3 {
-			http.Error(w, "No task name", http.StatusBadRequest)
-			return
-		}
-
-		taskName := taskSegments[2]
-
-		err := r.ParseForm()
-		if err != nil {
-			http.Redirect(w, r, "/", http.StatusSeeOther)
-			return
-		}
-
-		answer := r.FormValue("answer")
-
-		accept := false
-		for _, task := range tasks {
-			if task.Name == taskName && task.Answer == answer {
-				accept = true
-			}
-		}
-
-		if accept {
-			markTaskDone(user, taskName)
-		}
-
-		type SubmitPage struct {
-			TaskName string
-			Accepted bool
-		}
-
-		submitPage := SubmitPage{TaskName: taskName, Accepted: accept}
-		submit_tpl.Execute(w, submitPage)
-	}))
+	http.HandleFunc("/submit/", BasicAuth(SubmitPage))
 
 	http.ListenAndServe(":8080", nil)
 }
